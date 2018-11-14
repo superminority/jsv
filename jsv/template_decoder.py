8f441295e03beecfc04b2b7c17632822f74dee82
Alex Kovner <dev@alexkovner.com>
2018-11-14 17:12:06 -0500

    refactoring parse_record

--- jsv/template_decoder.py
+++ jsv/template_decoder.py
@@ -11,29 +11,24 @@ class TemplateStates(Enum):
     ARRAY_NEXT_OR_CLOSE = auto()
     OBJECT_AFTER_KEY = auto()
     OBJECT_NEXT_OR_CLOSE = auto()
+    EXPECT_QUOTE_OR_OBJECT_CLOSE = auto()
+    EXPECT_QUOTE = auto()
+
+
+@unique
+class StringStates(Enum):
     STRING_NEXT_OR_CLOSE = auto()
     STRING_ESCAPE = auto()
     STRING_HEX = auto()
-    EXPECT_QUOTE_OR_OBJECT_CLOSE = auto()
-    EXPECT_QUOTE = auto()
 
 
 @unique
 class RecordExpectedStates(Enum):
     EXPECT_OBJECT_START = auto()
     EXPECT_OBJECT_END = auto()
-    EXPECT_OBJECT_END_OBJECT_PARENT = auto()
-    EXPECT_OBJECT_END_ARRAY_PARENT = auto()
-    EXPECT_OBJECT_END_DONE = auto()
     EXPECT_ARRAY_START = auto()
     EXPECT_ARRAY_END = auto()
-    EXPECT_ARRAY_END_OBJECT_PARENT = auto()
-    EXPECT_ARRAY_END_ARRAY_PARENT = auto()
-    EXPECT_ARRAY_END_DONE = auto()
     EXPECT_VALUE = auto()
-    EXPECT_VALUE_ARRAY_PARENT = auto()
-    EXPECT_VALUE_OBJECT_PARENT = auto()
-    EXPECT_VALUE_DONE = auto()
     OBJECT_KEY = auto()
 
 
@@ -60,16 +55,122 @@ hex_re = compile('[0-9a-fA-F]')
 
 
 def json_remainder(s_array):
-    print(s_array)
-    s = ''.join(reversed(s_array)).lstrip()
-    print(s)
+    s = ''.join(reversed(s_array))
+    start_len = len(s)
+    s = s.lstrip()
+    end_len = len(s)
     d = json.JSONDecoder()
     v, r = d.raw_decode(s)
-    return v, list(reversed(s[r:].lstrip()))
+    for i in range(r + start_len - end_len):
+        s_array.pop()
+    return v
 
 
 def err_msg(msg, i, c):
     return '{0} @ index: {1}, character: {2}'.format(msg, i, c)
+
+
+def get_json_value(char_list):
+    return json_remainder(char_list)
+
+
+def get_json_string(char_list):
+    state = StringStates.STRING_NEXT_OR_CLOSE
+    string_array = []
+    i = -1
+
+    while True:
+        try:
+            i += 1
+            current_char = char_list.pop()
+        except IndexError:
+            raise IndexError(err_msg('End of string reached unexpectedly', i, current_char))
+
+        # ---------------------------
+        # State: STRING_NEXT_OR_CLOSE
+        # ---------------------------
+        if state is StringStates.STRING_NEXT_OR_CLOSE:
+            if current_char == '"':
+                return ''.join(string_array)
+            elif current_char == '\\':
+                state = StringStates.STRING_ESCAPE
+            else:
+                string_array.append(current_char)
+
+        # --------------------
+        # State: STRING_ESCAPE
+        # --------------------
+        elif state is StringStates.STRING_ESCAPE:
+            if current_char == '"':
+                string_array.append('"')
+                state = StringStates.STRING_NEXT_OR_CLOSE
+            elif current_char == '\\':
+                string_array.append('\\')
+                state = StringStates.STRING_NEXT_OR_CLOSE
+            elif current_char == '/':
+                string_array.append('/')
+                state = StringStates.STRING_NEXT_OR_CLOSE
+            elif current_char == 'b':
+                string_array.append('\b')
+                state = StringStates.STRING_NEXT_OR_CLOSE
+            elif current_char == 'f':
+                string_array.append('\f')
+                state = StringStates.STRING_NEXT_OR_CLOSE
+            elif current_char == 'n':
+                string_array.append('\n')
+                state = StringStates.STRING_NEXT_OR_CLOSE
+            elif current_char == 'r':
+                string_array.append('\r')
+                state = StringStates.STRING_NEXT_OR_CLOSE
+            elif current_char == 't':
+                string_array.append('\t')
+                state = StringStates.STRING_NEXT_OR_CLOSE
+            elif current_char == 'u':
+                hex_array = []
+                state = StringStates.STRING_HEX
+            else:
+                raise ValueError(err_msg('expecting valid escape character', i, current_char))
+
+        # -----------------
+        # State: STRING_HEX
+        # -----------------
+        elif state is StringStates.STRING_HEX:
+            if hex_re.search(current_char):
+                hex_array.append(current_char)
+                if len(hex_array) >= 4:
+                    string_array.append(bytearray.fromhex(''.join(hex_array)).decode())
+                    state = StringStates.STRING_NEXT_OR_CLOSE
+                else:
+                    hex_array.append(current_char)
+            else:
+                raise ValueError(err_msg('Expected a hex character ([0-9A-Fa-f])', i, current_char))
+
+
+def get_key_value_pair(char_list):
+    current_char = ''
+    while current_char != '"':
+        try:
+            current_char = char_list.pop()
+        except IndexError:
+            raise IndexError('error')
+
+        if not current_char.isspace() and not current_char == '"':
+            raise ValueError('error')
+
+    k = get_json_string(char_list)
+
+    current_char = ''
+    while current_char != ':':
+        try:
+            current_char = char_list.pop()
+        except IndexError:
+            raise IndexError('error')
+
+        if not current_char.isspace() and not current_char == ':':
+            raise ValueError('error')
+
+    v = get_json_value(char_list)
+    return k, v
 
 
 class Template:
@@ -79,31 +180,79 @@ class Template:
         j = 0
         i = -1
         rs = self._record_states
-        cont = True
 
-        while cont:
+        while True:
             try:
                 i += 1
                 current_char = char_list.pop()
             except IndexError:
                 raise IndexError(err_msg('End of string reached unexpectedly', i, current_char))
 
-            if rs[j] is RecordExpectedStates.EXPECT_ARRAY_START:
+            if rs[j][0] is RecordExpectedStates.EXPECT_ARRAY_START:
                 if current_char.isspace():
                     pass
                 elif current_char == '[':
                     stack.append([])
+                    j += 1
                 else:
                     raise ValueError('error')
 
-            if isinstance(rs[j], tuple) and rs[j][0] is RecordExpectedStates.EXPECT_ARRAY_END_ARRAY_PARENT:
+            elif rs[j][0] is RecordExpectedStates.EXPECT_ARRAY_END:
                 if current_char.isspace():
                     pass
                 elif current_char == ']':
                     tmp = stack.pop()
-                    stack[-1].append(tmp)
+                    if rs[j][1] is ParentStates.ARRAY:
+                        stack[-1].append(tmp)
+                    elif rs[j][1] is ParentStates.OBJECT:
+                        key = stack.pop()
+                        stack[-1][key] = tmp
+                    else:
+                        return tmp
+                    j += 1
                 elif current_char == ',':
-                    j = rs[j][1]
+                    j = rs[j][2]
+
+            elif rs[j][0] is RecordExpectedStates.EXPECT_OBJECT_START:
+                if current_char.isspace():
+                    pass
+                elif current_char == '{':
+                    stack.append({})
+                    j += 1
+                else:
+                    raise ValueError('error')
+
+            elif rs[j][0] is RecordExpectedStates.EXPECT_OBJECT_END:
+                if current_char.isspace():
+                    pass
+                elif current_char == '}':
+                    tmp = stack.pop()
+                    if rs[j][1] is ParentStates.ARRAY:
+                        stack[-1].append(tmp)
+                    elif rs[j][1] is ParentStates.OBJECT:
+                        key = stack.pop()
+                        stack[-1][key] = tmp
+                    else:
+                        return tmp
+                    j += 1
+                elif current_char == ',':
+                    k, v = get_key_value_pair(char_list)
+                    stack[-1][k] = v
+
+            elif rs[j][0] is RecordExpectedStates.OBJECT_KEY:
+                stack.append(rs[j][1])
+                j += 1
+
+            elif rs[j][0] is RecordExpectedStates.EXPECT_VALUE:
+                tmp = get_json_value(char_list)
+                if rs[j][1] is ParentStates.ARRAY:
+                    stack[-1].append(tmp)
+                elif rs[j][1] is ParentStates.OBJECT:
+                    key = stack.pop()
+                    stack[-1][key] = tmp
+                else:
+                    return tmp
+                j += 1
 
     @property
     def remainder(self):
@@ -242,7 +391,7 @@ class Template:
                         state = TemplateStates.EXPECT_ARRAY_OR_OBJECT
                     elif current_char == '}':
                         parent_stack.pop()
-                        record_states.append((RecordExpectedStates.EXPECT_VALUE_OBJECT_PARENT,
+                        record_states.append((RecordExpectedStates.EXPECT_VALUE,
                                               ParentStates.OBJECT))
                         if parent_stack:
                             if parent_stack[-1] is ParentStates.OBJECT:
@@ -285,59 +434,6 @@ class Template:
                     else:
                         raise ValueError(err_msg('Expecting `,` or `}`', i, current_char))
 
-                # ---------------------------
-                # State: STRING_NEXT_OR_CLOSE
-                # ---------------------------
-                elif state is TemplateStates.STRING_NEXT_OR_CLOSE:
-                    if current_char == '"':
-                        record_states.append((RecordExpectedStates.OBJECT_KEY,
-                                              ''.join(string_array)))
-                        state = TemplateStates.OBJECT_AFTER_KEY
-                    elif current_char == '\\':
-                        state = TemplateStates.STRING_ESCAPE
-                    else:
-                        string_array.append(current_char)
-
-                # --------------------
-                # State: STRING_ESCAPE
-                # --------------------
-                elif state is TemplateStates.STRING_ESCAPE:
-                    if current_char == '"':
-                        string_array.append('"')
-                    elif current_char == '\\':
-                        string_array.append('\\')
-                    elif current_char == '/':
-                        string_array.append('/')
-                    elif current_char == 'b':
-                        string_array.append('\b')
-                    elif current_char == 'f':
-                        string_array.append('\f')
-                    elif current_char == 'n':
-                        string_array.append('\n')
-                    elif current_char == 'r':
-                        string_array.append('\r')
-                    elif current_char == 't':
-                        string_array.append('\t')
-                    elif current_char == 'u':
-                        hex_array = []
-                        state = TemplateStates.STRING_HEX
-                    else:
-                        raise ValueError(err_msg('expecting valid escape character', i, current_char))
-
-                # -----------------
-                # State: STRING_HEX
-                # -----------------
-                elif state is TemplateStates.STRING_HEX:
-                    if hex_re.search(current_char):
-                        hex_array.append(current_char)
-                        if len(hex_array) >= 4:
-                            string_array.append(bytearray.fromhex(''.join(hex_array)).decode())
-                            state = TemplateStates.STRING_NEXT_OR_CLOSE
-                        else:
-                            hex_array.append(current_char)
-                    else:
-                        raise ValueError(err_msg('Expected a hex character ([0-9A-Fa-f])', i, current_char))
-
                 # ----------------------------
                 # State: EXPECT_QUOTE_OR_CLOSE
                 # ----------------------------
@@ -345,8 +441,10 @@ class Template:
                     if current_char.isspace():
                         pass
                     elif current_char == '"':
-                        string_array = []
-                        state = TemplateStates.STRING_NEXT_OR_CLOSE
+                        key_str = get_json_string(char_list)
+                        record_states.append((RecordExpectedStates.OBJECT_KEY,
+                                              key_str))
+                        state = TemplateStates.OBJECT_AFTER_KEY
                     elif current_char == '}':
                         parent_stack.pop()
                         if parent_stack:
@@ -372,8 +470,10 @@ class Template:
                     if current_char.isspace():
                         pass
                     elif current_char == '"':
-                        string_array = []
-                        state = TemplateStates.STRING_NEXT_OR_CLOSE
+                        key_str = get_json_string(char_list)
+                        record_states.append((RecordExpectedStates.OBJECT_KEY,
+                                              key_str))
+                        state = TemplateStates.OBJECT_AFTER_KEY
                     else:
                         raise ValueError('Expecting `"`')
         else:
