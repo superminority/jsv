2cbf9257f9e410274cb8404f7a198ae3c5a12c50
Alex Kovner <dev@alexkovner.com>
2018-12-06 11:01:58 -0500

    more work on template_set

--- jsv/template_set.py
+++ jsv/template_set.py
@@ -1,4 +1,4 @@
-from collections import OrderedDict
+import re
 from jsv.template import Template
 
 
@@ -13,7 +13,11 @@ class TemplateMultiWriter:
         self._template_dict = {}
         self._id_dict = {}
         self._tests = []
-        self._default_template = Template('')
+        self._default_template = {
+            'template': Template(''),
+            'written': False,
+            'default': True
+        }
 
     def __enter__(self):
         self._file_obj = open(self._file_name, self._mode)
@@ -24,41 +28,89 @@ class TemplateMultiWriter:
 
     def add_template(self, template, **kwargs):
         if 'id' in kwargs:
-            if kwargs['id']
-        if 'test' in kwargs:
+            id = kwargs['id']
+            if isinstance(id, str):
+                if validate_id(id):
+                    new_id = id
+                else:
+                    raise ValueError('Bad template id. Template id must match `{}`'.format(id_regex_str))
+            else:
+                raise TypeError('keyword argument `id` must be a string')
+        else:
+            new_id = get_new_id(self._id_dict)
 
-        if isinstance(template, str):
-            t = Template(template)
-        elif isinstance(template, Template):
+        if 'default' in kwargs:
+            if not isinstance(kwargs['default'], bool):
+                raise TypeError('keyword argument `default` must be a boolean')
+            elif kwargs['default'] and 'id' in kwargs:
+                raise ValueError('keyword argument `default` cannot be true if `id` is provided')
+            default = kwargs['default']
+        else:
+            default = False
+
+        if isinstance(template, Template):
             t = template
         else:
-            raise TypeError('`template` must be either a string or a Template object')
+            t = Template(template)
 
-        self._templates.update = {id: {
-            'template': t,
-            'test': test,
-            'id': id
-        }}
+        if default:
+            t_obj = {
+                'template': t,
+                'written': False,
+                'default': True
+            }
+            self._default_template = t_obj
+        else:
+            t_obj = {
+                'id': new_id,
+                'template': t,
+                'written': False,
+                'default': False
+            }
+            self._id_dict[new_id] = t_obj
+        self._template_dict[t] = t_obj
 
     def write(self, obj, **kwargs):
-        if len(kwargs) != 1:
-            raise KeyError('Either `template` or `id` keyword argument is required, but not both')
+        if 'template' in kwargs and 'id' in kwargs:
+            raise KeyError('Cannot include both `template` and `id` in keyword args')
         else:
             if 'template' in kwargs:
-                if not isinstance(kwargs['template'], Template):
-                    raise TypeError('template must be an object of type Template')
-
-        if id:
-            t = self._templates[id]
-        else:
-            t = self._default_template
-            for k, v in self._template.items():
-                if v['test'](obj):
-                    t = v['template']
-                    break
+                if isinstance(kwargs['template'], Template):
+                    t = kwargs['template']
+                else:
+                    t = Template(kwargs['template'])
+                try:
+                    t_obj = self._template_dict[t]
+                except KeyError:
+                    self.add_template(t)
+                    t_obj = self._template_dict[t]
+            elif 'id' in kwargs:
+                if kwargs['id'] in self._id_dict:
+                    t_obj = self._id_dict[kwargs['id']]
+                else:
+                    raise KeyError('id `{}` not found'.format(kwargs['id']))
+            else:
+                t_obj = self._default_template
                     
-        if id == '_':
-            print(t.encode(obj))
+        if t_obj['default']:
+            print(t_obj['template'].encode(obj))
         else:
-            print('@{0} {1}'.format(id, t.encode(obj)))
+            print('@{0} {1}'.format(t_obj['id'], t_obj['template'].encode(obj)))
 
+
+id_regex_str = '[a-zA-Z_0-9]+'
+id_re = re.compile(id_regex_str)
+
+
+def validate_id(id):
+    m = id_re.match(id)
+    return m.group() == id
+
+
+def get_new_id(id_dict):
+    i = 0
+    while True:
+        new_id = str(i)
+        if new_id not in id_dict:
+            return new_id
+        i += 1
