df71ab63a70e39c5c7c7cddc4a33f7075232971a
Alex Kovner <dev@alexkovner.com>
2018-12-06 15:24:01 -0500

    more work on template_set, renamed to io

--- jsv/io.py
+++ jsv/io.py
@@ -13,11 +13,16 @@ class TemplateMultiWriter:
         self._template_dict = {}
         self._id_dict = {}
         self._tests = []
-        self._default_template = {
+        self._file_obj = None
+
+        t_obj = {
+            'id': '_',
             'template': Template(''),
             'written': False,
             'default': True
         }
+        self._id_dict['_'] = t_obj
+        self._template_dict[t_obj['template']] = t_obj
 
     def __enter__(self):
         self._file_obj = open(self._file_name, self._mode)
@@ -25,6 +30,7 @@ class TemplateMultiWriter:
 
     def __exit__(self, type, value, traceback):
         self._file_obj.close()
+        self._file_obj = None
 
     def add_template(self, template, **kwargs):
         if 'id' in kwargs:
@@ -33,7 +39,8 @@ class TemplateMultiWriter:
                 if validate_id(id):
                     new_id = id
                 else:
-                    raise ValueError('Bad template id. Template id must match `{}`'.format(id_regex_str))
+                    raise ValueError(
+                        'Bad template id. Template id must match `{}`, and cannot be `_`'.format(id_regex_str))
             else:
                 raise TypeError('keyword argument `id` must be a string')
         else:
@@ -43,10 +50,11 @@ class TemplateMultiWriter:
             if not isinstance(kwargs['default'], bool):
                 raise TypeError('keyword argument `default` must be a boolean')
             elif kwargs['default'] and 'id' in kwargs:
-                raise ValueError('keyword argument `default` cannot be true if `id` is provided')
+                if (kwargs['default'] and (new_id != '_')) or ((not kwargs['default']) and (new_id == '_')):
+                    raise ValueError('keyword argument `default` and argument `id` are contradictory')
             default = kwargs['default']
         else:
-            default = False
+            default = new_id == '_'
 
         if isinstance(template, Template):
             t = template
@@ -55,11 +63,12 @@ class TemplateMultiWriter:
 
         if default:
             t_obj = {
+                'id': '_',
                 'template': t,
                 'written': False,
                 'default': True
             }
-            self._default_template = t_obj
+            new_id = '_'
         else:
             t_obj = {
                 'id': new_id,
@@ -67,10 +76,15 @@ class TemplateMultiWriter:
                 'written': False,
                 'default': False
             }
-            self._id_dict[new_id] = t_obj
+        self._id_dict[new_id] = t_obj
         self._template_dict[t] = t_obj
 
+        return t_obj['id'], t_obj['template']
+
     def write(self, obj, **kwargs):
+        if not self._file_obj:
+            raise RuntimeError('You must be inside the context manager to write')
+
         if 'template' in kwargs and 'id' in kwargs:
             raise KeyError('Cannot include both `template` and `id` in keyword args')
         else:
@@ -90,12 +104,16 @@ class TemplateMultiWriter:
                 else:
                     raise KeyError('id `{}` not found'.format(kwargs['id']))
             else:
-                t_obj = self._default_template
+                t_obj = self._id_dict['_']
+
+        if not t_obj['written']:
+            print('#{0} {1}'.format(t_obj['id'], str(t_obj['template'])), file=self._file_obj)
+            t_obj['written'] = True
                     
         if t_obj['default']:
-            print(t_obj['template'].encode(obj))
+            print(t_obj['template'].encode(obj), file=self._file_obj)
         else:
-            print('@{0} {1}'.format(t_obj['id'], t_obj['template'].encode(obj)))
+            print('@{0} {1}'.format(t_obj['id'], t_obj['template'].encode(obj)), file=self._file_obj)
 
 
 id_regex_str = '[a-zA-Z_0-9]+'
