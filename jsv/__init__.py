b348396ebd5b8163cd4d73e3b59505235fc78bf8
Alex Kovner <dev@alexkovner.com>
2018-12-14 14:34:08 -0500

    more work to get builds to reflect correct version

--- jsv/__init__.py
+++ jsv/__init__.py
@@ -1,3 +1,4 @@
 from .template import JSVTemplate, JSVRecordDecodeError, JSVTemplateDecodeError, JSVRecordEncodeError
 from .io import JSVCollection
-from .__version__ import __version__
+from .__version__ import __description__, __url__, __version__, __commit_hash__, __author__, __author_email__
+from .__version__ import __license__, __copyright__
